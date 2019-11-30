c6a60a64a0f098e16bb5ff138fb6e29a2c8f0666
Sarah <alahmadisr@gmail.com>
2019-11-30 14:11:19 +0300

    the game now keeps track of the players' scores

--- js/main.js
+++ js/main.js
@@ -9,7 +9,10 @@
 var player1={name:"player 1",
 color: "red",
 numberOfCirclesClicked:0, // a counter to keep tracking the number of circles already checked by the player
-numberOfRoundsWon:0};
+numberOfRoundsWon:0,
+ roundsWon : function() {
+   this.numberOfRoundsWon++;
+  }};
 
 //Player 2;
 var player2={name:"player 2",
@@ -17,7 +20,7 @@ color: "yellow",
 numberOfCirclesClicked:0,
 numberOfRoundsWon:0};
 
-var totalCirclesChecked=0;
+var totalCirclesChecked=0; //will use this variable to check for ties.
 
 var winningPatterns=['first-row','first-column','second-row','second-column','thirs-row','third-column',
 'forth-row','forth-column','first-diagonal','second-diagonal'];
@@ -26,6 +29,8 @@ var winningPatterns=['first-row','first-column','second-row','second-column','th
 
 //Let the first turn of the game be player 1's turn.
 
+function main(){
+newGame();
 var turn=player1;
 $(document).ready(function(){
 	$("[id*='circle-']").click(function(){
@@ -39,7 +44,21 @@ $(document).ready(function(){
 
       if(turn.numberOfCirclesClicked>=4){ //if the current player has alredy checked 4 circles or more, check for winning patterns.
 
-		checkForAWinningPattern(this);
+		if(checkForAWinningPattern(this)){
+		console.log(turn.name+" WINS!");
+      	$(".dot").off("click");
+      	turn.numberOfRoundsWon++;
+      	console.log("Player 1 has won: "+player1.numberOfRoundsWon+"\nPlayer 2 has won: "+player2.numberOfRoundsWon);
+      	alert(turn.name+" WINS!");
+      	var newGameButton=$("<br> <button> New Game </button>");
+      	$("#container").after(newGameButton);
+      	$("button").click(function(){
+		$("#game-board").empty();
+		main();
+
+
+		});
+		}
  //-----------------------------------------------------
 
 
@@ -74,7 +93,6 @@ function switchTurns(){
 function updateCounters(){
 	getCurrentPlayer().numberOfCirclesClicked++;
 	totalCirclesChecked++;
-
 }
 
 
@@ -105,9 +123,7 @@ function checkForAWinningPattern(currentC){
       					winningCirclesCounter++;
       					if(winningCirclesCounter==4){
       						console.log("Winning Pattern= "+winningPatterns[i]);
-      						console.log(turn.name+" WINS!");
-      						$(".dot").off("click");
-      						return;
+      						return true;
 
       					} //end of inner if statement.
       				} // end of outer if statement.
@@ -115,7 +131,7 @@ function checkForAWinningPattern(currentC){
       				else{
       					winningCirclesCounter=0;
       					break;
-      					return;
+      					return false;
       				} //end of else clause.
       			} // end of inner for loop (used to check each circle of the line).
       		} //end of switch case.
@@ -124,5 +140,44 @@ function checkForAWinningPattern(currentC){
 
 
 	}
+
+	function newGame(){
+		player1.numberOfCirclesClicked=0;
+		player2.numberOfCirclesClicked=0;
+		totalCirclesChecked=0;
+
+$("#game-board").append("<span class='dot first-row first-column first-diagonal' id='circle-1'></span>");
+$("#game-board").append("<span class='dot first-row second-column'id='circle-2'></span>");
+$("#game-board").append("<span class='dot first-row third-column' id='circle-3'></span>");
+$("#game-board").append("<span class='dot first-row forth-column second-diagonal' id='circle-4'></span>");
+$("#game-board").append("<br>");
+
+$("#game-board").append("<span class='dot second-row first-column' id='circle-5'></span>");
+$("#game-board").append("<span class='dot second-row second-column first-diagonal' id='circle-6'></span>");
+$("#game-board").append("<span class='dot second-row third-column second-diagonal' id='circle-7'></span>");
+$("#game-board").append("<span class='dot second-row forth-column' id='circle-8'></span>");
+
+$("#game-board").append("<br>");
+
+$("#game-board").append("<span class='dot third-row first-column' id='circle-9'></span>");
+$("#game-board").append("<span class='dot third-row second-column second-diagonal' id='circle-10'></span>");
+$("#game-board").append("<span class='dot third-row third-column first-diagonal' id='circle-11' ></span>");
+$("#game-board").append("<span class='dot third-row forth-column' id='circle-12'></span>");
+
+$("#game-board").append("<br>");
+
+$("#game-board").append("<span class='dot forth-row first-column second-diagonal' id='circle-13'></span>");
+$("#game-board").append("<span class='dot forth-row second-column' id='circle-14'></span>");
+$("#game-board").append("<span class='dot forth-row third-column' id='circle-15'></span>");
+$("#game-board").append("<span class='dot forth-row first-diagonal forth-column' id='circle-16'></span>");
+
+$(".dot").on("click");
+$("button").remove();
+
+
+	}
+}
+
+main();
 
 

f5c1caee23aabad804397db2ec0cd3b9c92e3c53
Sarah <alahmadisr@gmail.com>
2019-11-30 06:59:12 +0300

    added players related variables and enabled turns

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
 
-//An array to hold players information.
-var players=[
+
 
 //Player 1;
-{name:"player1",
+var player1={name:"player1",
 color: "red",
-numberOfRoundsWon:0},
+numberOfRoundsWon:0};
 
 //Player 2;
-{name:"player2",
+var player2={name:"player2",
 color: "yellow",
-numberOfRoundsWon:0},
+numberOfRoundsWon:0};
 
-];
+
 
 
 var numberOfCirclesClicked1=0; // a counter to keep tracking the number of circles already checked by player1.
@@ -20,10 +19,25 @@ var numberOfCirclesClicked2=0; // a counter to keep tracking the number of circl
 //we will use the counters above as a condition to start checking for winning.
 //The game should check for winners only if either player have 4 circles or more of the board checked.
 
+//Let the first turn of the game be player 1's turn.
+var turn=player1;
 $(document).ready(function(){
 	$("[id*='circle-']").click(function(){
-      $(this).addClass("red");
-
+		var color=(turn.color).toString();
+		console.log(color);
+      $(this).addClass(color);
+      $(this).off("click"); //Once the circle is checked, the player can no loger interact with it again.
+      switchTurns(); //Give the turn to the other player.
         });
 
 });
+
+
+function switchTurns(){
+	if (turn==player1){
+		turn=player2;
+	}
+	else{
+		turn=player1;
+	}
+}
